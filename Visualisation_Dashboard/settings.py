f78dbd08a496ab018d85d1dd6e2acbd4b1300651
PramodKumavat7758 <78492409+PramodKumavat7758@users.noreply.github.com>
2024-12-03 15:25:10 +0530

    Update settings.py

--- Visualisation_Dashboard/settings.py
+++ Visualisation_Dashboard/settings.py
@@ -136,11 +136,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+"""
 STATIC_URL = '/static/'
 STATICFILES_DERS = [
     os.path.join(BASE_DIR, 'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+"""
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]  # Adjust path if needed
+STATIC_ROOT = BASE_DIR / "staticfiles"   # Used for `collectstatic` in production
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
